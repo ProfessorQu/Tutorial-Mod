29d9bad503e0c5462a477751f7b3f8a87d1cefe7
ProfessorQu <quint@dasookwat.com>
2022-08-13 12:46:08 +0200

    Added amethyst ore and amethyst blocks
    Added amethyst ore that drops amethyst,
    Added amethyst blocks,
    Added crafting recipes with amethyst blocks,

--- src/main/java/com/professorqu/tutorialmod/item/ModItems.java
+++ src/main/java/com/professorqu/tutorialmod/item/ModItems.java
@@ -9,8 +9,8 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class ModItems {
-    public static final DeferredRegister<Item> ITEMS =
-            DeferredRegister.create(ForgeRegistries.ITEMS, TutorialMod.MOD_ID);
+    public static final DeferredRegister<Item> ITEMS
+            = DeferredRegister.create(ForgeRegistries.ITEMS, TutorialMod.MOD_ID);
 
     public static final RegistryObject<Item> AMETHYST = ITEMS.register("amethyst",
             () -> new Item(new Item.Properties().group(ModItemGroup.TUTORIAL_GROUP)));
